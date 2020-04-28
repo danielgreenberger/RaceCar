9725365a1b487f458337b88c5aa32f906e3ce588
Daniel Greenberger <admin@yehudi.org.il>
2020-04-28 18:04:13 +0300

    Update ros_tf_publisher.h

--- RosIntegration/ros_tf_publisher.h
+++ RosIntegration/ros_tf_publisher.h
@@ -54,6 +54,13 @@ using string = std::string;
 template <typename T>
 class TimeframePublisher : public Publisher<T>
 {
+private:
+    std::shared_ptr<tf::TransformBroadcaster> m_p_broadcaster;  // TODO: consider moving both back to inside function
+    std::shared_ptr<tf::TransformListener> m_p_listener;
+    
+    
+
+
 public:    
    /*=======================================================
     * @brief           Simple constructor for the publisher class
@@ -80,7 +87,6 @@ public:
     
     
 
-
 //--------------------------------------------------------------------------
 //                           Publisher interface functions
 //--------------------------------------------------------------------------
@@ -105,50 +111,24 @@ private:
         
         /* Register to publish for topic */
         ros::Publisher node_publisher = node_handler.advertise<T>(this->get_topic_name(), 100);
-        tf::TransformBroadcaster broadcaster;
 
-        
+        /* Init TF publisher and listener */
+        m_p_listener = std::make_shared<tf::TransformListener> (ros::Duration(10));
+      //  m_p_broadcaster = std::make_shared<tf::TransformBroadcaster> ();  // TODO: publisher/listener should belong to node
+
         
         /* Define publish frequency */
-        ros::Rate loop_rate(ROS_PUBLISH_RATE_PER_SECOND);
-
+        ros::Rate publisher_rate_per_second(10);
+        const int tf_rate_loop = 10;
         
-        
-        /* Publish */
+        /* Publish in loop */
         int count = 0;
         while ( this->active() )
+            
         {
             
-            // Generate transform
+            publish_frame_transform();
             
-         //   auto rotation = tf::Quaternion(0, 0, 0, 1);
-            auto translation_vector = tf::Vector3(
-                                            0.0,    /* x */
-                                            0.0,    /* y */
-                                            0.0     /* z */
-                                          );
-                                          
-            auto rotation_matrix = tf::Matrix3x3    (
-                                                        1,   0,   0,
-                                                        0,   1,   0,
-                                                        0,   0,   1 
-                                                    );	
-                                          
-                                          
-                                          
-            auto transform = tf::Transform(rotation_matrix, translation_vector);
-            
-            auto tf_transform = tf::StampedTransform(
-                                                    transform,
-                                                    ros::Time::now(),
-                                                    "base_link", 
-                                                    "base_laser"
-                                                    );
-                
-            
-            broadcaster.sendTransform(tf_transform);
-
-                
             // Pull a message from FIFO
             T msg;
             bool msg_exists = this->pull_message_if_exists(msg);
@@ -165,24 +145,102 @@ private:
 
 
             // Wait for next publish interval 
-            loop_rate.sleep();
+            publisher_rate_per_second.sleep();
+            
+       //     transformPoint();
             
         }
         
+        
+        
+        
+        
+        
     }
-  
-};
+    
+    
+    
+    
+    /*=======================================================
+    * @brief           TODO
+    *
+    * @description     TODO
+    *
+    * @param           TODO
+    *
+    * @return          TODO
+    *
+    * @author          TODO
+    =========================================================*/
+    void publish_frame_transform()
+    {
+        
+        m_p_broadcaster = std::make_shared<tf::TransformBroadcaster> ();  // TODO: publisher/listener should belong to node
+       
+        std::cout << "Publishing TF transform" <<std::endl;
+        
+        /* Transform:  Base -> Camera */
+        auto transform = tf::Transform(RSENSE_TO_BASE_ROTATION, RSENSE_TO_BASE_TRANSLATION);
+        auto tf_transform = tf::StampedTransform(
+                                                transform,
+                                                ros::Time::now(),
+                                                BASE_FRAME_ID,
+                                                CAMERA_FRAME_ID
+                                                );
+        m_p_broadcaster->sendTransform(tf_transform);
+                                      
+
+        /* Transform:  Camera -> Bitcraze */
+        transform = tf::Transform(BCRAZE_TO_RSENSE_ROTATION, BCRAZE_TO_RSENSE_TRANSLATION);
+        tf_transform = tf::StampedTransform(
+                                              transform,
+                                              ros::Time::now(),
+                                              CAMERA_FRAME_ID,
+                                              ODOMETER_FRAME_ID 
+                                              );
+        m_p_broadcaster->sendTransform(tf_transform);
+
+    }
+
+
+//--------------------------------------------------------------------------
+//                           Publisher interface functions
+//--------------------------------------------------------------------------
+//      void transformPoint()
+//      {
+//         // We'll create a point in the odometer frame that we'd like to transform to the base frame
+//         geometry_msgs::PointStamped odom_point;
+//         
+//         odom_point.header.frame_id = ODOMETER_FRAME_ID;
+//         odom_point.header.stamp = ros::Time();
+//         odom_point.point.x = 1.0;
+//         odom_point.point.y = 1.0;
+//         odom_point.point.z = 1.0;
+//   
+//   
+//         try
+//         {
+//          geometry_msgs::PointStamped base_point;
+//          m_p_listener->transformPoint(CAMERA_FRAME_ID, odom_point, base_point);
+//   
+//          ROS_INFO("odometer: (%.2f, %.2f. %.2f) -----> base: (%.2f, %.2f, %.2f) at time %.2f",
+//              odom_point.point.x, odom_point.point.y, odom_point.point.z,
+//              base_point.point.x, base_point.point.y, base_point.point.z, base_point.header.stamp.toSec());
+//         }
+//         catch(tf::TransformException& ex)
+//         {
+//              ROS_ERROR("Received an exception trying to transform a point from \"odometer\" to \"base\": %s", ex.what());
+//              ASSERT(false, tf::TransformException("Received an exception trying to transform a point"));
+//         }
+//      }
 
 #undef ROS_PUBLISHER_CRITICAL_SECTION
 
-//    ros::Rate r(100);
-//   
-//    tf::TransformBroadcaster broadcaster;
-//   
+
+
+};
 
 }
-
-
 
 
 #endif /* ROS_COMPILATION */
