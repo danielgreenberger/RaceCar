728c778f9d84f0f057bc5f665dc8f24e3036aa53
Daniel Greenberger <admin@yehudi.org.il>
2020-05-03 21:38:57 +0300

    Update Bitcraze_linear_to_angular.h

--- Common/Coordinates/Bitcraze_linear_to_angular.h
+++ Common/Coordinates/Bitcraze_linear_to_angular.h
@@ -2,7 +2,10 @@
 #define BCRZ_LINEAR_TO_ROT
 
 #include <cmath>
-        
+#include <utility>
+
+namespace OdometerTransform
+{ 
 /*
 					
 					Calculation:  Rotation by t from Bitcraze linear data
@@ -21,6 +24,8 @@
 					 | /                   
 					 |/_ _ _ _ _ _ _ _ _ _ _ _ _ Y                   
 					 
+
+-----------  Calculation of t ----------------
 			
 			t is the angle of rotation.
 			
@@ -28,20 +33,20 @@
 		    (Dy)/(Dr) = sin(t)	
 			
 		
-		    For Dy = 0 we get  t = 0  since  Dr = Dx 
+		    For Dy = 0:  
+            =>   t = 0  since  Dr = Dx 
 		   
      	    For Dy != 0:
-				(Dx)/(Dy) = cos(t) / sin(t)  = cot(t)	
+			=>	(Dx)/(Dy) = cos(t) / sin(t)  = cot(t)	
 			=> 	t = arccot (Dx/Dy)
+			=>  t = PI/2 - arctan(Dx/Dy)     (Since for every angle w ->  arccot(w) = PI/2 - arctan(w):)
 			
-			Since for every angle w ->  arccot(w) = PI/2 - arctan(w):
-			=>  t = PI/2 - arctan(Dx/Dy)
-			
+
 */
 
 inline double get_angle_from_linear_offset(const double Dx, const double Dy)
 {
-    int angular_movement_radians;
+    double angular_movement_radians;
     
     if (Dy == 0)
     {
@@ -51,9 +56,87 @@ inline double get_angle_from_linear_offset(const double Dx, const double Dy)
     {
         angular_movement_radians = M_PI/2 - atan(Dx/Dy);
     }
+    
+    return angular_movement_radians;
 }
+
+
+
+
+
+
+
+
+/*
+					
+					Calculation:  Convert Dx'  Dy'  from current odometer frame to Dx Dy, which are the X-Y offset in the lab frame 
+					
+
+                     X           X'        					 
+                     |          /            
+					 |  ->     / 
+					 |        /  
+					 |       /   
+					 |      /    
+			    (Dx) |     /        
+					 |    /      
+					 | t /       
+					 |  /        
+					 | /  90-t       
+					 |/_ _ _ _ _ _ _ _ _ _ _ _ _ Y                   
+                     |\  t                                      					 
+                     | \                                       					 
+                     |  \                                      					 
+                     |   \                                     					 
+                     |90-t\
+                     |     \
+                     |      \
+                     |       \
+                     |      t \
+                     |_ _ _ _ _\
+                                Y'   (Perpendicular to X')                                   					 
+                      
+
+                      
+                                                              					 
+        X = X' * cos(t)  -  Y' * sin(t)
+`       Y = X' * sin(t)  +  Y' * cos(t)        
+                                                              					 
+                                                              					 
+                                                              					 
+                                                              					 
+
+*/
+
+
+
+/*=======================================================
+* @brief           Converts distance travelled from   X'-Y'  to  X-Y  frame.
+*
+* @description     See the diagram above which justifies the calculation. 
+*
+* @param           t  -  The angle by which X' and Y'  are rotated  
+*
+* @param           r_Dx   -  The distance in X' units
+*
+* @param           r_Dy   -  The distance in Y' units
+*
+* @return          A pair (Dx,Dy)  representing the difference in X and Y  (lab) frame.
+*
+* @author          Daniel Greenberger
+=========================================================*/
+using DistXY = std::pair<double,double>;
+inline DistXY convert_dist_to_lab_frame (const double t, const double r_Dx, const double r_Dy)
+{
+    const double dx = r_Dx*cos(t) - r_Dy*sin(t);
+    const double dy = r_Dx*sin(t) + r_Dy*cos(t);
+    
+    return std::make_pair(dx,dy);
+}
+
+ 
 
 			
 					
-
+}       //  namespace OdometerTransform
 #endif  // BCRZ_LINEAR_TO_ROT
