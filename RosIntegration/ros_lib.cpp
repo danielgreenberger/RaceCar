2328fb1fc9b4b11bc18c0be5d86a484906ec2999
Daniel Greenberger <admin@yehudi.org.il>
2020-06-04 18:02:28 +0300

    Update ros library functions

--- RosIntegration/ros_lib.cpp
+++ RosIntegration/ros_lib.cpp
@@ -37,47 +37,10 @@ CAMERA_DEFINITION
 /////            FUNCTIONS            /////
 ///////////////////////////////////////////
 
+#ifdef ENABLE_EXPERIMENTAL_PCL_FUNCTIONS
+
 CAMERA_DEFINITION
 (
-
-    static inline uint8_t* GetColorPixel(const rs2::video_frame color_frame, const rs2::texture_coordinate* texture_point)
-    {
-        ASSERT(texture_point, std::exception());
-        
-        /* Get color frame metadata */
-        uint32_t color_width = color_frame.get_width();    // Image width in pixels
-        uint32_t color_height = color_frame.get_height();  // Image height in pixels
-        uint32_t color_bytes_per_pixel = color_frame.get_bytes_per_pixel(); 
-               
-        /* Get color frame data */
-        uint8_t* color_data = (uint8_t*)color_frame.get_data(); 
-        
-        /* Get texture indices */
-        // dgreenbe todo explain meaning of i and j
-        float i(texture_point->u);
-        float j(texture_point->v);
-        
-        bool texture_valid = (i >= 0.f && i <= 1.f && j >= 0.f && j <= 1.f);
-        ASSERT(texture_valid, std::exception());
-       
-       
-        /* Calculate pixel offset in data */
-        if (texture_valid) 
-        {
-            int pixel_x = static_cast<int>(i * color_width);
-            int pixel_y = static_cast<int>(j * color_height);
-            
-            int base_offset_pixels = (pixel_y * color_width) + pixel_x;
-            int base_offset_bytes = base_offset_pixels * color_bytes_per_pixel;
-            
-            return color_data + base_offset_bytes;
-        }
-        else
-        {
-            return NULL;
-        }
-    }
-
 
     sensor_msgs::PointCloud2 PointCloudConversion(rs2::points pc) 
     {
@@ -125,112 +88,11 @@ CAMERA_DEFINITION
     }
 
 
-    sensor_msgs::PointCloud2 PointCloudConversion(rs2::points pc, const rs2::video_frame color_frame)
-    {  
 
-          /////////////////////////////////////////////////////
-          //         Get realsense PointCloud data           //
-          /////////////////////////////////////////////////////
-          
-          int total_points = static_cast<int>(pc.size());
-          auto ros_tsf = ros::Time::now(); // dgreenbe TODO use frame tsf instead?
-          
-          const rs2::vertex* points_array = pc.get_vertices();
-          const rs2::texture_coordinate* texture_points_array = pc.get_texture_coordinates();
-          
-          
-          ///////////////////////////////////////////////////////////////
-          //         Create PointCloud2 msg and define fields          //
-          ///////////////////////////////////////////////////////////////
-          
-          // Create a PointCloud2
-          sensor_msgs::PointCloud2 pcl_msg;
-          
-          // Fill some internals of the PoinCloud2 like the header/width/height ...
-          pcl_msg.height = 1;  
-          pcl_msg.width = total_points;
-          pcl_msg.header.stamp = ros_tsf; 
-          pcl_msg.header.frame_id = tf_realsense_frame_id; 
-          pcl_msg.is_dense = false;
-          
-          
-          // Set the point fields to xyzrgb and resize the vector with the following command.
-          // 4 is for the number of added fields. 
-          // Each field comes in triplet: 
-          //      (1) the name of the PointField,
-          //      (2) the number of occurrences of the type in the PointField
-          //      (3) the type of the PointField
-          sensor_msgs::PointCloud2Modifier modifier(pcl_msg);
-          modifier.setPointCloud2Fields(
-                                            4,     
-                                            "x", 1, sensor_msgs::PointField::FLOAT32,
-                                            "y", 1, sensor_msgs::PointField::FLOAT32,
-                                            "z", 1, sensor_msgs::PointField::FLOAT32,
-                                            "rgb", 1, sensor_msgs::PointField::FLOAT32
-                                       );
-                                                   
-
-          
-          
-          ////////////////////////////////////////////////////////////
-          //         traverse PointCloud using an iterator          //
-          ////////////////////////////////////////////////////////////
-
-          // Define the iterators
-          sensor_msgs::PointCloud2Iterator<float> iter_x(pcl_msg, "x");
-          sensor_msgs::PointCloud2Iterator<float> iter_y(pcl_msg, "y");
-          sensor_msgs::PointCloud2Iterator<float> iter_z(pcl_msg, "z");
-          
-          // Even though the r,g,b,a fields do not exist (it's usually rgb, rgba), we can create iterators for
-          // them. they will handle data packing for you (in little endian RGB is packed as *,R,G,B in a float
-          // and RGBA as A,R,G,B)
-          sensor_msgs::PointCloud2Iterator<uint8_t> iter_r(pcl_msg, "r");
-          sensor_msgs::PointCloud2Iterator<uint8_t> iter_g(pcl_msg, "g");
-          sensor_msgs::PointCloud2Iterator<uint8_t> iter_b(pcl_msg, "b");
-          
-          
-          // Fill the PointCloud2
-          for(size_t i=0; i<total_points; ++i) 
-          {
-            const rs2::vertex* current_point = &points_array[i];
-            const rs2::texture_coordinate* current_point_texture = &texture_points_array[i];
-            
-            // Filter bad points
-            // dgreenbe TODO
-          
-            // Fill point data
-            *iter_x = current_point->x;
-            *iter_y = current_point->y;
-            *iter_z = current_point->z;
-            
-            // Fill color data
-            uint8_t* current_pixel_p;
-            const ColorPixel* current_pixel = reinterpret_cast<const ColorPixel*>(GetColorPixel(color_frame, current_point_texture));
-            *iter_r = current_pixel->r;
-            *iter_g = current_pixel->g;
-            *iter_b = current_pixel->g;
-            
-            // Advance iterators
-            ++iter_x; 
-            ++iter_y; 
-            ++iter_z; 
-            ++iter_r; 
-            ++iter_g; 
-            ++iter_b;
-          }
-          
-          /* Resize */
-          // TODO after adding filter
-          // modifier.resize(valid_points); // dgreenbe TODO is necessarry?
-          
-          /* Finish */
-          return pcl_msg;
-          
-    }
 
 )
 
-
+#endif  // ENABLE_EXPERIMENTAL_PCL_FUNCTIONS
 
 
 
--- RosIntegration/ros_lib.h
+++ RosIntegration/ros_lib.h
@@ -2,12 +2,15 @@
 #define _ROS_JETSON_UTILS_H_
 
 /*
+    @AUTHOR  Daniel Greenberger  (david.daniel.greenberger@gmail.com)
+    
 	@DESCRIPTION
 	
-	TODO
+	This file contains miscellaneous library functions which can be used with under ROS.
 */
 
 #ifdef ROS_COMPILATION
+
 
 
 
@@ -27,6 +30,17 @@
 /////////////////////////////////////
 
 
+/*
+*      Experimental functions
+*     
+*      For some functions in this file, we were not able to validate their input. 
+*      We decided to still include them under compilation flag, in hopes the code 
+*      could still be useful at some point in the future. 
+*                         
+*      BEFORE USING THE FUNCTIONS BELOW, MAKE SURE TO VALIDATE THAT POINTCLOUD REPRESENTS A VALID OUTPUT.     
+*
+*/
+//#define ENABLE_EXPERIMENTAL_PCL_FUNCTIONS
 
 
 /////////////////////////////////////////
@@ -38,19 +52,21 @@ namespace RosIntegration
 
 	
 
-#ifndef NO_CAMERA
-#warn "The outout of PointCloudConversion has not been verified."
-    /*=======================================================
-    * @brief           Converts RealSense pointcloud to ROS PointCloud2 message
-    *
-    * @param           pc         -  RealSense point cloud.
-    *
-    * @return          ROS PointCloud2 msg
-    =========================================================*/
-    sensor_msgs::PointCloud2 PointCloudConversion(rs2::points pc);
-    sensor_msgs::PointCloud2 PointCloudConversion(rs2::points pc, const rs2::video_frame color_frame);
+#ifdef ENABLE_EXPERIMENTAL_PCL_FUNCTIONS
+    #ifndef NO_CAMERA
+    #warn "The outout of PointCloudConversion has not been verified."
+        /*=======================================================
+        * @brief           Converts RealSense pointcloud to ROS PointCloud2 message
+        *
+        * @param           pc         -  RealSense point cloud.
+        *
+        * @return          ROS PointCloud2 msg
+        =========================================================*/
+        sensor_msgs::PointCloud2 PointCloudConversion(rs2::points pc);
+        sensor_msgs::PointCloud2 PointCloudConversion(rs2::points pc, const rs2::video_frame color_frame);
 
-#endif
+    #endif  /* NO_CAMERA */
+#endif  /* ENABLE_EXPERIMENTAL_PCL_FUNCTIONS */
 
 
 
